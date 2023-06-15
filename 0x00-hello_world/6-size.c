6decf73c6dd59644c0f5b86526714872222c7bbf
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-15 10:13:54 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - A program that prints the size of various computer types
  * Return: 0 (success)
  */
 int main(void)
 {
-char a
+char a;
 int b;
 long int c;
 long long int d;
