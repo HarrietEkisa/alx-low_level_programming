dfc203ea719d1c39199cac2d0d56567a2646f5d4
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-16 14:08:41 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+prinf("\n");
+}
