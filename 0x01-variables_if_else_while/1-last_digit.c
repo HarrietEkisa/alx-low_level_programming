83630ebdbecb18a99e37afda6677e9ce9621d186
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-16 11:02:01 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d and  is greater than 5\n", n, lastnum);
 }
 else if (lastnum < 6 && lastnum != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
 }
 return (0);
 }
