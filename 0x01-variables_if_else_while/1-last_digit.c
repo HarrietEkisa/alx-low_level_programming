f9e7f43afe7a218b298a61c6727a91f16c6b35a1
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-16 11:07:12 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,8 +14,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastnum = n % 10;
 if (lastnum > 5)
-{
-printf("Last digit of %d and  is greater than 5\n", n);
+printf("Last digit of %d is greater than 5\n", n);
 }
 else if (lastnum == 0)
 {
