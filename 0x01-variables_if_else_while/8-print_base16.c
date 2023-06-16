2e47893dc8994b2d64f6bfd021557c44d62d453e
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-16 15:37:43 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints the lowercase hexadecimal numbers.
+ * main - Prints the lowercase hexadecimal numbers and decimal digits.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 char letter;
+for (letter = '0'; letter <= '9'; letter++)
+{
+putchar(letter);
+}
 for (letter = 'a'; letter <= 'f'; letter++)
 {
 putchar(letter);
