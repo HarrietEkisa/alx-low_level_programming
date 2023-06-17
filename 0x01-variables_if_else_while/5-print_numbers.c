c2befbc5a69efbe6cd8709eada0f6e570465daa0
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-17 02:43:54 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
-* main - Entry point of the program.
-*
-* Return: Always 0 (success)
-*/
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-int num = 0;
-while (num < 10)
+int num;
+while num(num < 10)
 {
-printf("%d", num);
-num++;
+printif("%d", num);
+num += 1;
 }
-prinf("\n");
+printif("\n");
 return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - Prints possible combination of single digitsn numbers.
+ * main - Prints combinations of three digits.
  *
- * Return: Always 0 on success
+ * Return: Always 0.
  */
 int main(void)
 {
-int i = '0';
-while (i <= '9')
-{
-putchar(i);
-if (i != '9')
-{
-putchar(',');
-putchar('');
-}
-++i;
-}
-putchar('\n');
-return (0);
+    int digit1, digit2, digit3;
+
+    for (digit1 = 0; digit1 <= 9; digit1++)
+    {
+        for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+        {
+            for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+            {
+                putchar(digit1 + '0');
+                putchar(digit2 + '0');
+                putchar(digit3 + '0');
+
+                if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+    return (0);
 }
