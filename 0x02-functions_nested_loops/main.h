e12a50607219f8e15a497c6dc742a690388c4e3c
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-20 12:55:23 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,5 @@
-main.h
-
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char);
 void print_alphabet(void);
@@ -18,4 +16,3 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
-
