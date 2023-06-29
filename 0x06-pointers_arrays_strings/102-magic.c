be96ceb340bbdc4881a5e6bb35e9d1c08da5f68e
HarrietEkisa <harrietekisa@gmaiil.com>
2023-06-29 11:47:04 -0700

    A program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,12 +16,15 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* The line of code below uses pointer arithmetic to indirectly modify a[2].
-* We subtract 2 from the index of a[2] to get the correct offset from p.
-* Since p points to n, *(p - 2) will access the memory location of a[2].
-* Then we assign the value 98 to that memory location.
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
-*(p - 2) = 98;
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
